543ef1df4668b65dea7fa3d85f4f3878a70d55dd
sw5012 <105565235+sw5012@users.noreply.github.com>
2022-05-16 01:13:28 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -73,21 +73,21 @@ int main() {
 		pressure = getPressure();
 		thread_sleep_for(88);
 		if(pressure>=170 && pressure - last_pressure<0 && !starting_flag) {  // pressure reaches 150, and start to releasing pressure
-			starting_flag = true;
+			starting_flag = true;  // start the experiment
 			printf("experiment starts and stop increase the pressure./n");
 			thread_sleep_for(2000);
-		} else if(pressure<=30 && starting_flag) {
+		} else if(pressure<=30 && starting_flag) {  // end phase
 			starting_flag = false;
 			// Analyze data 
 			float SBP_diff = max_pressure_diff*0.55; // using Om to calculate Os https://patents.google.com/patent/CN102018507A/en
 			float DBP_diff = max_pressure_diff*0.82; // using Om to calculate Od https://patents.google.com/patent/CN102018507A/en
-			for(int i=index;i>=0;i--) {
-				if((diff_data[i] > 0) && (diff_data[i]<SBP_diff)) {
-					SBP = data[i];
+			for(int i=index;i>=0;i--) {  // staring from MAP, find the index of Os and get the corresponded pressure data
+				if((diff_data[i] > 0) && (diff_data[i]<SBP_diff)) {  // find the index of Os
+					SBP = data[i];  // get the corresponded pressure
 					break;
 				}
 			}
-			for(int i = index;i<diff_data.size()-1;i++) {
+			for(int i = index;i<diff_data.size()-1;i++) {  // same as SBP, find and Od and DBP
 				if((diff_data[i] > 0) && (diff_data[i]<DBP_diff)) {
 					DBP = data[i];
 					break;
@@ -100,11 +100,24 @@ int main() {
 					break;
 				}
 			}
-			//float sbp = 
+			// print result
 			printf("Mean arterial pressure is: %f.\n",MAP);
 			printf("Systolic Blood pressure is: %f.\n",SBP);
 			printf("Diastolic Blood pressure is: %f.\n",DBP);
 			printf("Heart Rate is: %d.\n", heart_rate);
+
+			// Analyze blood pressure, ref:https://www.heart.org/en/health-topics/high-blood-pressure/understanding-blood-pressure-readings
+			if(SBP>=180||DBP>=120) {
+				printf("This stage of high blood pressure requires medical attention. If your blood pressure readings suddenly exceed 180/120 mm Hg, wait five minutes and then test your blood pressure again. If your readings are still unusually high, contact your doctor immediately. You could be experiencing a hypertensive crisis.\n");
+			} else if(SBP>=140||DBP>=90) {
+				printf("Hypertension Stage 2 is when blood pressure consistently ranges at 140/90 mm Hg or higher. At this stage of high blood pressure, doctors are likely to prescribe a combination of blood pressure medications and lifestyle changes.\n");
+			} else if(SBP>=130||DBP>=80) {
+				printf("Hypertension Stage 1 is when blood pressure consistently ranges from 130-139 systolic or 80-89 mm Hg diastolic. At this stage of high blood pressure, doctors are likely to prescribe lifestyle changes and may consider adding blood pressure medication based on your risk of atherosclerotic cardiovascular disease (ASCVD), such as heart attack or stroke.\n");
+			} else if(SBP>=120 && DBP<80) {
+				printf("Elevated blood pressure is when readings consistently range from 120-129 systolic and less than 80 mm Hg diastolic. People with elevated blood pressure are likely to develop high blood pressure unless steps are taken to control the condition.\n");
+			} else {
+				printf("Blood pressure numbers of less than 120/80 mm Hg are considered within the normal range. If your results fall into this category, stick with heart-healthy habits like following a balanced diet and getting regular exercise.\n");
+			}
 			data.clear(); // clear data for the next experitment
 			diff_data.clear();
 			counter = 0;
@@ -112,18 +125,18 @@ int main() {
 			// time for observe the result
 			thread_sleep_for(10000);
 		} else if(starting_flag) { // start the experiment
-			pressure_diff = pressure - last_pressure;
-			diff_data.push_back(pressure_diff);
-			data.push_back(pressure);
+			pressure_diff = pressure - last_pressure; // record the pressure difference between current and last
+			diff_data.push_back(pressure_diff); // record the difference
+			data.push_back(pressure);  // record current pressure
 			if(pressure_diff>max_pressure_diff) {
 				max_pressure_diff = pressure_diff; // update max pressure diff
 				MAP = pressure; // update MAP
-				index = counter;
+				index = counter; // index of MAP
 			}
 			avg_pressure_diff += pressure_diff;
 			counter ++; // update counter
 			if(counter%10==0) { // test release rate once per second
-				printf("The current Pressure is %f.\n", avg_pressure_diff);
+				printf("The current Pressure is %f.\n", pressure);
 
 				if(avg_pressure_diff>=-2) {
 					printf("warnning, the release rate is too slow!\n");
@@ -135,13 +148,6 @@ int main() {
 		} else {
 			printf("Below the start pressure! The current pressure is %f.\n", pressure);
 		}
-		// if(pressure < 150) {
-		// 	// wait for 1000ms if the pressure does not meet the threshold
-		// 	thread_sleep_for(1000);
-		// 	continue;
-		// } else {
-		// 	// start sampling after reaching 150mmHg
-		// }
 	}
 
 }
